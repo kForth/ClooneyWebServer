16d2a2919f5ce07a54e04521deb44953a5e7413d
Kestin Goforth <kgoforth1503@gmail.com>
2017-03-22 15:05:43 -0400

    add sql stuff

--- server/__init__.py
+++ server/__init__.py
@@ -1,38 +1,62 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+from flask_security import SQLAlchemyUserDatastore, Security
 from tba_py import BlueAllianceAPI
+import better_exceptions
 
 from server.data import DataServer
 from server.db import Database
 from server.stats import StatsServer
 from server.info import InfoServer
 from server.receiver import ReceiverServer
+from server.users import UsersServer
+from server.sql import SqlServer
 
-from predict.opr import OprCalculator
+app = Flask(__name__, static_folder='../server/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/db.sqlite'  # 'postgres://localhost:5432'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "D4&u$VDtwe2Ng!q&"
+sql_db = SQLAlchemy(app)
 
 
 class ClooneyServer(object):
-    def __init__(self, name, config):
-        self.app = Flask(name, static_folder='./server/static')
-        self.config = config
+    def __init__(self):
+        self.app = app
+        self.sql_db = sql_db
+
+        from server.models import OprEntry, User, Role
+
+        self.user_datastore = SQLAlchemyUserDatastore(sql_db, User, Role)
+        self.security = Security(app, self.user_datastore)
+
+        self.sql_db.create_all()
+
+        selfapi_manager = APIManager(app, flask_sqlalchemy_db=sql_db)
+        selfapi_manager.create_api(OprEntry, methods=['GET'], results_per_page=-1, url_prefix="/api/sql/")
+
         self.tba = BlueAllianceAPI('kestin_goforth', 'Clooney', '1.0', enable_caching=True, cache_db_path='./tba.json')
         self.db = Database()
-        self.opr_calc = OprCalculator(self.tba)
 
         self._register_views()
         self.data_server = DataServer(self._add, "/api")
-        self.stats_server = StatsServer(self._add, self.db, self.tba, "/api")
+        self.stats_server = StatsServer(self._add, self.db, self.sql_db, self.tba, "/api")
         self.info_server = InfoServer(self._add, self.db, self.tba, "/api")
+        self.sql_server = SqlServer(self._add, self.sql_db, "/api/sql")
         self.recv_server = ReceiverServer(self._add, self.tba, self.db, url_prefix="/api/data")
+        self.user_server = UsersServer(self._add, self.user_datastore, self.security)
 
         self.run = self.app.run
 
     def _register_views(self):
         self._add('/', self.index)
+        self._add('/opr/', self.opr)
         self._add('/s/', self.settings)
         self._add('/a/', self.analysis)
+        self._add('/account/', self.account)
 
     def _add(self, route: str, func: classmethod, methods=('GET',), url_prefix=""):
-        self.app.add_url_rule(url_prefix + route, view_func=func, methods=methods)
+        self.app.add_url_rule(url_prefix + route, route, view_func=func, methods=methods)
 
     def index(self):
         return self.app.send_static_file('views/index.html')
@@ -42,3 +66,31 @@ class ClooneyServer(object):
 
     def settings(self):
         return self.app.send_static_file('views/settings/index.html')
+
+    def account(self):
+        return self.app.send_static_file('views/login.html')
+
+    def opr(self):
+        return self.app.send_static_file('views/oprs.html')
+
+
+if __name__ == "__main__":
+    import json
+    from server.models import ScoutingEntry
+
+    event_id = "2017onto2"
+    data = json.load(open("../clooney/data/{}/raw_data.json".format(event_id)))
+
+    for line in data:
+        fields = {
+            'filename': "",
+            'data': json.dumps(line),
+            'team': int(line["team_number"]),
+            'match': int(str(line["match"]).strip("*")),
+            'pos': int(line["pos"]),
+            'event': event_id
+        }
+        entry = ScoutingEntry(**fields)
+        sql_db.session.add(entry)
+    sql_db.session.commit()
+
---
+++ server/sql.py
@@ -0,0 +1,36 @@
+from flask import make_response, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+import json
+
+
+class SqlServer(object):
+    def __init__(self, add: classmethod, sql_db: SQLAlchemy, url_prefix=""):
+        self._add = lambda *x, **y: add(*x, **y, url_prefix=url_prefix)
+        self.db = sql_db
+        self._register_views()
+
+    def _register_views(self):
+        self._add('/opr/events', lambda: self._get_unique_values('oprs', 'event'))
+        self._add('/opr/teams', lambda: self._get_unique_values('oprs', 'team'))
+        self._add('/add_entry', self.add_scouting_entry, methods=["POST"])
+
+    def _get_unique_values(self, table, column):
+        query = "SELECT DISTINCT %s FROM %s"
+        data = (column, table)
+        result = self.db.engine.execute(query, data)
+        unique_list = list(map(lambda x: x[0], result))
+        return make_response(jsonify(unique_list), 200)
+
+    def add_scouting_entry(self):
+        from server.models import ScoutingEntry
+        data = request.json
+        print(request.json)
+        data["data"] = json.dumps(data["data"])
+        entry = ScoutingEntry(**data)
+        self.db.session.add(entry)
+        self.db.session.commit()
+        return make_response(jsonify({}), 200)
+
+    def get_event_oprs(self, event_id):
+        return
