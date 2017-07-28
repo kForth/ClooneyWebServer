0139e7acbe36b2ea604fa5fcc0f6be5a89b873da
Kestin Goforth <kgoforth1503@gmail.com>
2017-07-28 19:13:21 -0400

    initial sql model commit

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -3,3 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from server import app
 
 sql_db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    from server.db.models import *
+    sql_db.create_all()
---
+++ server/db/models.py
@@ -0,0 +1,64 @@
+from sqlalchemy.dialects.postgresql import JSON
+
+from server.db import sql_db as db
+
+
+event_teams = db.Table('event_teams',
+    db.Column('team_number', db.Integer, db.ForeignKey('team.number')),
+    db.Column('event_key', db.Integer, db.ForeignKey('event.key'))
+)
+
+match_teams = db.Table('match_teams',
+    db.Column('team_number', db.Integer, db.ForeignKey('team.number')),
+    db.Column('match_id', db.Integer, db.ForeignKey('match.id'))
+)
+
+
+class Event(db.Model):
+    event_key = db.Column(db.String(16), primary_key=True)
+    info = db.Column(db.Text)
+    year = db.Column(db.Integer)
+
+    matches = db.relationship('Match', backref='event', lazy='dynamic')
+    scouting_entries = db.relationship('ScoutingEntry', backref='event', lazy='dynamic')
+    teams = db.relationship('Team', secondary=event_teams, backref=db.backref('event', lazy='dynamic'))
+
+    def __repr__(self):
+        return '<Event {}>'.format(self.id)
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    match_number = db.Column(db.Integer)
+    match_level = db.Column(db.String(16))
+    event_key = db.Column(db.String(16), db.ForeignKey('event.key'))
+
+    scouting_entries = db.relationship('ScoutingEntry', backref='match', lazy='dynamic')
+    teams = db.relationship('Team', secondary=match_teams, backref=db.backref('match', lazy='dynamic'))
+
+    def __repr__(self):
+        return "<Match {0}{1} @ {2}>".format(self.match_number, self.match_level, self.event.key)
+
+
+class Team(db.Model):
+    number = db.Column(db.Integer, primary_key=True)
+    info = db.Column(JSON)
+
+    scouting_entries = db.relationship('ScoutingEntry', backref='team', lazy='dynamic')
+    events = db.relationship('Event', secondary=event_teams, backref=db.backref('team', lazy='dynamic'))
+    matches = db.relationship('Match', backref='team', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Team {}>'.format(self.team_num)
+
+
+class ScoutingEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(16), db.ForeignKey('event.key'))
+    team_number = db.Column(db.Integer, db.ForeignKey('team.number'))
+    match_number = db.Column(db.Integer, db.ForeignKey('match.match_number'))
+    match_id = db.Column(db.Integer, db.ForeignKey('match.id'))
+    value = db.Column(JSON)
+
+    def __repr__(self):
+        return '<ScoutingEntry {0}:{1} [{2}]>'.format(self.event, self.team.number, self.key)
