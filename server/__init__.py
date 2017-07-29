870398dff7c8d0e755c747a8d9a5a9efefe64884
Kestin Goforth <kgoforth1503@gmail.com>
2017-07-29 18:08:36 -0400

    create tba wrapper instance for app

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
+from tba import TBA
+
 app = Flask(__name__, static_folder='../static')
 app.config.from_pyfile('../server.cfg')
+tba = TBA('KestinGoforth', 'Clooney', '3')
