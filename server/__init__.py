044778be4b202ee474d84defe3236760730b09a0
Kestin Goforth <kgoforth1503@gmail.com>
2017-08-29 16:43:44 -0400

    add and server index page

--- server/__init__.py
+++ server/__init__.py
@@ -5,3 +5,8 @@ from tba import TBA
 app = Flask(__name__, static_folder='../static')
 app.config.from_pyfile('../server.cfg')
 tba = TBA(app.config['TBA_AUTH_KEY'])
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('views/index.html')
