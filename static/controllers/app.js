e344c353e89a3723256924ce5a16b4c3ca590d14
Kestin Goforth <kgoforth1503@gmail.com>
2017-11-09 22:29:10 -0500

    cleanup

--- static/controllers/app.js
+++ static/controllers/app.js
@@ -1,14 +1,27 @@
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'angular-md5', 'chart.js'])
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'angular-md5', 'ui.sortable', 'chart.js'])
     .config(function ($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(false).hashPrefix('');
         $routeProvider
             .when('/', {
                 templateUrl: '../../../static/views/pages/home.html',
-                controller: 'MainViewController'
+                controller: 'HomeController'
             })
-            .when('/sheets', {
-                templateUrl: '../../../static/views/pages/create_sheets.html',
-                controller: 'SheetCreatorController'
+            .when('/a', {
+                templateUrl: '../../../static/views/pages/averages.html',
+                controller: 'AveragesController'
+            })
+            .when('/i', {
+                templateUrl: '../../../static/views/pages/insights.html',
+                controller: 'InsightsController'
+            })
+            .when('/m', {
+                templateUrl: '../../../static/views/pages/matches.html',
+                controller: 'MatchesController'
+            })
+            .when('/e', {
+                templateUrl: '../../../static/views/pages/entries.html',
+                controller: 'EntriesController'
+            })
             .when('/s/c', {
                 templateUrl: '../../../static/views/pages/config/edit_calculations.html',
                 controller: 'ConfigController'
@@ -18,397 +31,71 @@ var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'angula
             });
     });
 
-app.controller('ApplicationController', function () {
-
+app.controller('ApplicationController', function ($scope, $cookies) {
+    if($cookies.get('tracked_event') != undefined){
+        $scope.tracked_event = $cookies.get('tracked_event');
+    }
 });
 
-app.controller('MainViewController', function () {
-
+app.controller('SidebarController', function ($scope, $cookies, $location) {
+    if($cookies.get('tracked_team') != undefined){
+        $scope.tracked_team = $cookies.get('tracked_team');
+    }
+    if($cookies.get('tracked_event') != undefined){
+        $scope.event_name = $cookies.get('tracked_event');
+    }
+    $scope.nav = function(path){
+        $location.path(path);
+        $cookies.put('tracked_team', $scope.tracked_team);
+    }
 });
 
-app.directive('fieldSettingsDirective', function () {
-    function link($scope) {
-        $scope.show_contents = true;
-        $scope.toggle_contents = function () {
-            $scope.show_contents = !$scope.show_contents;
-        };
+app.controller('HomeController', function ($scope, $location, $cookies) {
+    $scope.selected_event = "";
+    $scope.events = [{'key': '2017onwa', 'name': 'Waterloo'}, {'key': '2017onham', 'name': 'McMaster'}, {'key': '2017obar', 'name': 'Georgian'}]
 
-    $scope.get_field_settings = function (field_type) {
-        switch (field_type) {
-            default:
-                return {};
-            case 'Numbers':
-                return [
-                    {
-                        'name': 'Label',
-                        'type': 'text',
-                        'id': 'label',
-                        'default': ''
-                    },
-                    {
-                        'name': 'Max Value',
-                        'type': 'number',
-                        'id': 'ones',
-                        'default': 9
-                    },
-                    {
-                        'name': '\'+10\'s',
-                        'type': 'number',
-                        'id': 'tens',
-                        'default': 1
-                    },
-                    {
-                        'name': 'Show Zero',
-                        'type': 'checkbox',
-                        'id': 'show_zero',
-                        'default': false
-                    },
-                    {
-                        'name': 'Allow Prev Line',
-                        'type': 'checkbox',
-                        'id': 'prev_line',
-                        'default': false
-                    },
-                    {
-                        'name': 'Prev Line Offset',
-                        'type': 'number',
-                        'id': 'offset',
-                        'default': 0
-                    },
-                    {
-                        'name': 'Add Note Space',
-                        'type': 'checkbox',
-                        'id': 'note_space',
-                        'default': false
-                    },
-                    {
-                        'name': 'Note Space Width',
-                        'type': 'number',
-                        'id': 'note_width',
-                        'default': 0
-                    }
-                ]
-        }
-    };
+    if($cookies.get('tracked_event') != undefined){
+        $scope.selected_event = $cookies.get('tracked_event').name;
     }
 
-    var directive = {
-        restrict: 'C',
-        scope: {
-            field: '='
-        },
-        link: link,
-        templateUrl: '../../static/views/templates/field_settings.html'
-        // template: ''
+    $scope.select_event = function(){
+        if($scope.selected_event == "") return;
+        console.log($scope.selected_event);
+        $scope.events.forEach(function(elem){
+            if(elem.name === $scope.selected_event){
+                $cookies.put('tracked_event', elem.key);
+                $scope.tracked_event = elem.key;
+                $location.path("/a");
+            }
+        });
     };
-
-    return directive;
-
-    // directive.compile = function (element, attributes) {
-    //     // element.css("border", "1px solid #cccccc");
-    //
-    //     //linkFunction is linked with each element with scope to get the element specific data.
-    //     var linkFunction = function ($scope, element, attributes) {
-    //         // element.html("Student: <b>"+$scope.student.name +"</b> , Roll No: <b>"+$scope.student.rollno+"</b><br/>");
-    //         // element.css("background-color", "#ff00ff");
-    //     };
-    //     return linkFunction;
-    // };
 });
 
-app.controller('SheetCreatorController', function ($scope, $cookies) {
-    // if($cookies.get('fields_data') != undefined){
-    //     $scope.data = $cookies.get('fields_data');
-    // }
-    // else{
-        $scope.data = [];
-    // }
+app.controller('AveragesController', function ($scope) {
 
-    $scope.add_field_status = {
-        isopen: false
-      };
+});
 
-    $scope.add_field = function(){
-        var settings = $scope.get_field_settings('Numbers');
-        var obj = {
-            'id': '',
-            'type': 'Numbers',
-            'options': {}
-        };
-        for(var e in settings){
-            e = settings[e];
-            obj['options'][e.id] = e.default;
-        }
-        $scope.data.push(obj);
-        $cookies.put('fields_data', $scope.data);
-        console.log($scope.data);
-    };
+app.controller('InsightsController', function ($scope) {
 
-    $scope.get_field_settings = function (field_type) {
-        switch (field_type) {
-            default:
-                return {};
-            case 'Numbers':
-                return [
-                    {
-                        'name': 'Label',
-                        'type': 'text',
-                        'id': 'label',
-                        'default': ''
-                    },
-                    {
-                        'name': 'Max Value',
-                        'type': 'number',
-                        'id': 'ones',
-                        'default': 9
-                    },
-                    {
-                        'name': '\'+10\'s',
-                        'type': 'number',
-                        'id': 'tens',
-                        'default': 1
-                    },
-                    {
-                        'name': 'Show Zero',
-                        'type': 'checkbox',
-                        'id': 'show_zero',
-                        'default': false
-                    },
-                    {
-                        'name': 'Allow Prev Line',
-                        'type': 'checkbox',
-                        'id': 'prev_line',
-                        'default': false
-                    },
-                    {
-                        'name': 'Prev Line Offset',
-                        'type': 'number',
-                        'id': 'offset',
-                        'default': 0
-                    },
-                    {
-                        'name': 'Add Note Space',
-                        'type': 'checkbox',
-                        'id': 'note_space',
-                        'default': false
-                    },
-                    {
-                        'name': 'Note Space Width',
-                        'type': 'number',
-                        'id': 'note_width',
-                        'default': 0
-                    }
-                ]
-        }
-    };
+});
 
-    // $scope.data = [
-    //     {
-    //         "name": "Draw the Robot",
-    //         "type": "Image",
-    //         "options": {
-    //             "label": "Draw the Robot",
-    //             "width": 2,
-    //             "height": 1.5,
-    //             "image_path": null,
-    //             "prev_line": true,
-    //             "offset": 3.75,
-    //             "y_offset": 1.25
-    //         },
-    //         "id": "robot_drawing"
-    //     },
-    //     {
-    //         "name": "Auto Divider",
-    //         "type": "Divider",
-    //         "options": {
-    //             "label": "Autonomous"
-    //         }
-    //     },
-    //     {
-    //         "name": "Auto Didn't Move",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "Didn't Move"
-    //         },
-    //         "id": "auto_no_move"
-    //     },
-    //     {
-    //         "name": "Auto Scored Low Boiler",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "Scored Low Boiler",
-    //             "prev_line": true
-    //         },
-    //         "id": "auto_scored_low"
-    //     },
-    //     {
-    //         "name": "Auto Triggered Hopper",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "Triggered Hopper",
-    //             "prev_line": true
-    //         },
-    //         "id": "auto_triggered_hopper"
-    //     },
-    //     {
-    //         "name": "Auto Scored High",
-    //         "type": "Numbers",
-    //         "options": {
-    //             "label": "Scored High",
-    //             "tens": 5
-    //         },
-    //         "id": "auto_scored_high"
-    //     },
-    //     {
-    //         "name": "Auto Gear Location",
-    //         "type": "MultipleChoice",
-    //         "options": {
-    //             "label": "Gear Location",
-    //             "options": [
-    //                 "NA",
-    //                 "Blr",
-    //                 "Mid",
-    //                 "RZ"
-    //             ]
-    //         },
-    //         "default": "NA",
-    //         "id": "auto_gear_location"
-    //     },
-    //     {
-    //         "name": "Auto Gears Scored",
-    //         "type": "Numbers",
-    //         "options": {
-    //             "label": "Gears Scored",
-    //             "ones": 3,
-    //             "prev_line": true
-    //         },
-    //         "id": "auto_gears_scored",
-    //         "prev_line": true
-    //     },
-    //     {
-    //         "name": "Tele-Op Divider",
-    //         "type": "Divider",
-    //         "options": {
-    //             "label": "Tele-Op"
-    //         }
-    //     },
-    //     {
-    //         "name": "Gears Placed",
-    //         "type": "Numbers",
-    //         "options": {
-    //             "label": "Placed Gears",
-    //             "tens": 1,
-    //             "note_space": true
-    //         },
-    //         "id": "tele_gears_scored"
-    //     },
-    //     {
-    //         "name": "Gears From Near",
-    //         "type": "Numbers",
-    //         "options": {
-    //             "label": "Gears from Near",
-    //             "tens": 1,
-    //             "note_space": true
-    //         },
-    //         "id": "tele_gears_from_near"
-    //     },
-    //     {
-    //         "name": "Gears From Far",
-    //         "type": "Numbers",
-    //         "options": {
-    //             "label": "Gears from Far",
-    //             "tens": 1,
-    //             "note_space": true
-    //         },
-    //         "id": "tele_gears_from_far"
-    //     },
-    //     {
-    //         "name": "Scored High",
-    //         "type": "MultipleChoice",
-    //         "options": {
-    //             "label": "Scored High",
-    //             "options": [
-    //                 ["💩", 0],
-    //                 ["👎", 1],
-    //                 ["👍", 2],
-    //                 ["🔥", 3]
-    //             ]
-    //         },
-    //         "id": "tele_scored_high",
-    //         "default": "NA"
-    //     },
-    //     {
-    //         "name": "Performace Divider",
-    //         "type": "Divider",
-    //         "options": {
-    //             "label": "Overall Performance"
-    //         }
-    //     },
-    //     {
-    //         "name": "Sopped Moving",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "Stopped Moving"
-    //         },
-    //         "id": "disabled"
-    //     },
-    //     {
-    //         "name": "Never Moved",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "Never Moved",
-    //             "prev_line": true
-    //         },
-    //         "id": "no_move"
-    //     },
-    //     {
-    //         "name": "No Show",
-    //         "type": "Boolean",
-    //         "options": {
-    //             "label": "No Show",
-    //             "prev_line": true
-    //         },
-    //         "id": "no_show"
-    //     },
-    //     {
-    //         "name": "Climbed",
-    //         "type": "MultipleChoice",
-    //         "options": {
-    //             "label": "Climbed",
-    //             "options": [
-    //                 ["S", 1],
-    //                 ["F", 0]
-    //             ]
-    //         },
-    //         "id": "tele_climbed",
-    //         "default": "NA"
-    //     },
-    //     {
-    //         "name": "Defense",
-    //         "type": "MultipleChoice",
-    //         "options": {
-    //             "label": "Defense",
-    //             "options": [
-    //                 ["💩", 0],
-    //                 ["👎", 1],
-    //                 ["👍", 2],
-    //                 ["🔥", 3]
-    //             ],
-    //             "prev_line": true
-    //         },
-    //         "id": "tele_defense",
-    //         "default": ""
-    //     },
-    //     {
-    //         "name": "Notes",
-    //         "type": "Image",
-    //         "options": {
-    //             "label": "Notes",
-    //             "width": 5.625,
-    //             "height": 2.5,
-    //             "image_path": null
-    //         },
-    //         "id": "notes"
-    //     }
-    // ];
+app.controller('MatchesController', function ($scope) {
+
+});
+
+app.controller('EntriesController', function ($scope) {
+
+});
+
+app.controller('EditCalculationsController', function ($scope) {
+    $scope.calculations = [
+        {'name': 'a', 'key': 'a', 'formula':'x*x', 'type': 'float'},
+        {'name': 'b', 'key': 'b', 'formula':'x+y', 'type': 'float'},
+        {'name': 'c', 'key': 'c', 'formula':'x+y*x-y/x/x', 'type': 'float'},
+        {'name': 'd', 'key': 'd', 'formula':'x+2', 'type': 'float'}
+    ];
+});
+
+app.controller('ConfigController', function ($scope) {
+
 });
