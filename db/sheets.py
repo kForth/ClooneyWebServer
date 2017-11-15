313b16916d74e5d56761db7ebb2dc12233337460
Kestin Goforth <kgoforth1503@gmail.com>
2017-11-15 00:54:00 -0500

    add default field api endpoint

--- db/sheets.py
+++ db/sheets.py
@@ -1,4 +1,6 @@
-from flask import make_response, jsonify
+import json
+
+from flask import jsonify, make_response
 
 
 class SheetDatabaseInteractor:
@@ -6,7 +8,12 @@ class SheetDatabaseInteractor:
         self._db = db
         self._app = app
 
+        self.default_fields = json.load(open(app.root_path + "/field_settings.json"))
         self._app.add_url_rule('/get/sheets', '/get/sheets', self.get_sheets, methods=('GET',))
+        self._app.add_url_rule('/get/default_fields', '/get/default_fields', self.get_default_fields, methods=('GET',))
 
     def get_sheets(self):
         return make_response(jsonify(self._db.get_sheets()), 200)
+
+    def get_default_fields(self):
+        return make_response(jsonify(self.default_fields), 200)
