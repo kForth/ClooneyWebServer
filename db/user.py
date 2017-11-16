8af1731a62e0e2619b564e7d267a4a09abea6878
Kestin Goforth <kgoforth1503@gmail.com>
2017-11-16 16:46:23 -0500

    cleanup and fix passwords

--- db/user.py
+++ db/user.py
@@ -10,17 +10,17 @@ class UserDatabaseInteractor:
         self._db = db
         self._app = app
 
-        self._app.add_url_rule('/login', '/login', view_func=self.login, methods=('POST',))
-        self._app.add_url_rule('/users/create/', '/users/create/', self.create_user, methods=('POST',))
+        self._app.add_url_rule('/login', '/login', view_func=self.login_user, methods=('POST',))
+        self._app.add_url_rule('/users/create/', '/users/create/', self.register_user, methods=('POST',))
         self._app.add_url_rule('/users/update/<id>', '/users/update/<id>', self.get_user_by_name, methods=('POST',))
 
     def _encrypt_password(self, pwd):
-        hash = hmac.new(self._app.config['PASSWORD_KEY'].encode('UTF-8'), msg=pwd, digestmod=sha1)
+        hash = hmac.new(str(self._app.config['PASSWORD_KEY']).encode('UTF-8'), msg=str(pwd).encode('UTF-8'), digestmod=sha1)
         return hash.hexdigest()
 
     def _verify_password(self, pwd, hash):
-        pwd = str(self._encrypt_password(pwd))
-        return hmac.compare_digest(str(pwd), str(hash))
+        pwd = self._encrypt_password(pwd)
+        return hmac.compare_digest(str(pwd).encode('UTF-8'), str(hash).encode('UTF-8'))
 
     def get_user_by_id(self, id):
         user = self._db.get_user_by_id(id)
@@ -36,10 +36,10 @@ class UserDatabaseInteractor:
             return make_response(jsonify(user), 200)
         return make_response(jsonify(), 404)
 
-    def create_user(self):
+    def register_user(self):
         user = request.json
         if 'username' in user.keys():
-            existing = self._db.get_user_by_name(user['username'])
+            existing = self._db.get_user_by_name(user['username'].lower())
             if not existing:
                 if 'password' in user.keys():
                     user['role'] = self.ROLES[0]
@@ -50,21 +50,22 @@ class UserDatabaseInteractor:
         return make_response(jsonify(), 400)
 
     def update_user(self, id):
-        users = self._db.get_users()
         user = request.json()
-        if 'username' in user.keys() and len(user['username']) > 4 and user['username'] not in users.keys():
-            if 'password' in user.keys() and len(user['password']) > 5:
+        existing = self._db.get_user_by_id(id)
+        if existing:
+            if self._verify_password(user['password'], existing['password']):
                 self._db.update_user(id, user)
                 self._db.commit()
                 return make_response(jsonify(), 200)
         return make_response(jsonify(), 400)
 
-    def login(self):
+    def login_user(self):
         credentials = request.json
         if all([e in credentials.keys() for e in ['username', 'password']]):
             username = credentials['username']
             password = credentials['password']
             user = self._db.get_user_by_name(username)
+            print(user)
             if user and self._verify_password(password, user['password']):
                 response = {
                     'id': user['id'],
