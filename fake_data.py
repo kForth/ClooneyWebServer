228e822697af0ae7ef67eb84f310ce8a587cf64f
Kestin Goforth <kgoforth1503@gmail.com>
2017-11-20 22:40:45 -0500

    small changes

--- fake_data.py
+++ fake_data.py
@@ -6,9 +6,10 @@ import requests
 if __name__ == "__main__":
     event_key = '2017onham'
     db = json.load(open('db/db.json'))
-    sheet = list(db['sheets']['sheets'][1]["data"])
+    sheet = list(db['sheets']['sheets'][0]["data"])
     matches = db['events'][event_key]['matches']
 
+    i = 0
     for match in matches:
         for alliance in sorted(match['alliances'].keys()):
             for team in match['alliances'][alliance]['team_keys']:
@@ -18,12 +19,12 @@ if __name__ == "__main__":
                     'match': match['match_number'],
                     'pos': match['alliances'][alliance]['team_keys'].index('frc' + team) + (3 if alliance == 'blue' else 0)
                 }
-                print(team)
+                print('{}_{}_{}'.format(data['match'], data['pos'], data['team']))
                 for field in sheet:
                     if field['type'] == 'HorizontalOptions':
                         value = random.choice(field['options'].split(" "))
                     elif field['type'] == 'Numbers':
-                        max = field['ones'] + 10 * field['tens']
+                        max = (field['ones'] if 'ones' in field.keys() else 0) + 10 * (field['tens'] if 'tens' in field.keys() else 0)
                         value = random.randint(0, max)
                     elif field['type'] == 'Boolean':
                         value = True if random.randint(0, 1) == 1 else False
@@ -36,4 +37,6 @@ if __name__ == "__main__":
                     'filename': 'sheet_1.jpg'
                 }
                 resp = requests.post('http://0.0.0.0:5002/add/entry', json=payload)
-                print(resp)
+                if resp.status_code != 200:
+                    print(resp)
+
