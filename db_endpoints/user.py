22ed0f7fb20ef3733d611a8d0f6b05fb2d624146
Kestin Goforth <kgoforth1503@gmail.com>
2017-12-05 21:36:37 -0500

    add test_user for cookie login

--- db_endpoints/user.py
+++ db_endpoints/user.py
@@ -17,8 +17,18 @@ class UserDatabaseEndpoints:
         add_route('/logout', self.logout_user, ('POST',))
         add_route('/users/create/', self.register_user, ('POST',))
         add_route('/users/update/<id>', self.get_user_by_username, ('POST',))
+        add_route('/test_user', self.test_user, ('POST',))
         add_route('/get/user_settings/<username>', self.get_user_settings)
         add_route('/get/user_settings/', self.get_user_settings)
+
+    def test_user(self):
+        user = request.json
+        if user['id'] in self._active_users.keys() and self._active_users[user['id']] == user['key']:
+            user = self._db_interactor.get_user_by_id(user['id'])
+            return make_response(jsonify(user), 200)
+        elif user['user']['role'] == 'Guest' or user['user']['role'] == 0:
+            return make_response("", 204)
+        return make_response("", 401)
 
     def get_user_settings(self, username=None):
         if username:
@@ -35,7 +45,7 @@ class UserDatabaseEndpoints:
             user = user.to_json()
             user['password'] = ''
             return make_response(jsonify(user), 200)
-        return make_response(jsonify(), 404)
+        return make_response("", 404)
 
     def get_user_by_username(self, username):
         user = self._db_interactor.get_user_by_username(username.low)
@@ -43,7 +53,7 @@ class UserDatabaseEndpoints:
             user = user.to_json()
             user['password'] = ''
             return make_response(jsonify(user), 200)
-        return make_response(jsonify(), 404)
+        return make_response("", 404)
 
     def register_user(self):
         user_data = request.json
@@ -55,8 +65,8 @@ class UserDatabaseEndpoints:
                 user.role = self.ROLES[0]
                 user.password = self._db_interactor.encrypt_password(user.password)
                 self._db_interactor.add_user(user)
-                return make_response(jsonify(), 200)
-        return make_response(jsonify(), 400)
+                return make_response("", 200)
+        return make_response("", 400)
 
     def update_user(self, id):
         user = User.from_json(request.json())
@@ -64,8 +74,8 @@ class UserDatabaseEndpoints:
         if user and existing:
             if self._db_interactor.verify_password(user.password_hash, existing.password_hash):
                 self._db_interactor.update_user(id, user)
-                return make_response(jsonify(), 200)
-        return make_response(jsonify(), 400)
+                return make_response("", 200)
+        return make_response("", 400)
 
     def login_user(self):
         credentials = request.json
@@ -80,7 +90,7 @@ class UserDatabaseEndpoints:
                     'user': {
                         'id': user.id,
                         'username': username,
-                        'role': self.ROLES.index(user.role),
+                        'role': user.role,
                         'first_name': user.first_name,
                         'last_name': user.last_name
                     }
@@ -91,9 +101,9 @@ class UserDatabaseEndpoints:
                 }
                 return make_response(jsonify(response), 200)
             else:
-                return make_response(jsonify(), 400)
+                return make_response("", 400)
         else:
-            return make_response(jsonify(), 409)
+            return make_response("", 409)
 
     def logout_user(self):
         try:
